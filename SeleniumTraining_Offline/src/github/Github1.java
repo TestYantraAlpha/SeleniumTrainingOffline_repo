833bd00d72629acd4ca1a1750beceb7c6d57e1c0
QSP <QSP@DESKTOP-BKE9C99>
2024-01-02 12:25:59 +0530

    push

---
+++ SeleniumTraining_Offline/src/github/Github1.java
@@ -0,0 +1,17 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Github1 {
+	
+	@Test
+	public void test()
+	{
+		Reporter.log("pushed  msg",true);
+	}
+	
+	
+	
+
+}
