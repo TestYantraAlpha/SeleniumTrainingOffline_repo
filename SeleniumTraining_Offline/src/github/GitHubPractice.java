e63f55d86fa0c1409fe60a6c640bc94a220a7bfb
Abhijith1998 <123438717+Abhijith1998@users.noreply.github.com>
2024-01-02 11:43:03 +0530

    GitHubPractice class added...

---
+++ SeleniumTraining_Offline/src/github/GitHubPractice.java
@@ -0,0 +1,11 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GitHubPractice {
+	@Test
+	public void test() {
+		Reporter.log("GitHubPractice class added...",true);
+	}
+}
